b14097cfe7a6de8d6c207f1d6dca5baa4f5a5923
Nena Streeter <nenastreeter@gmail.com>
2021-03-25 16:04:15 -0500

    counter and last names uwu

--- app.js
+++ app.js
@@ -2,17 +2,46 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      lastName: '',
+    //  fullname: '',
     };
+  },
+
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        this.counter = 0;
+        setTimeout(function(){
+          that.counter = 0;
+        }, 2000);
+      }
+    }
+  //  name(value) {
+  //    if (value === '') {
+  //      this.fullname = '';
+  //    } else {
+  //      this.fullname = value + ' ' + this.lastName;
+  //    }
+      
+  //  },
+  //},
+  //lastName(value) {
+  //  if (value === '') {
+  //    this.fullname = '';
+  //  } else {
+  //    this.fullname = this.name + ' ' + value;
+  //  }
+    
   },
 
   computed: {
     fullname() {
       console.log('Running again...')
-      if (this.name === '') {
+      if (this.name === '' || this.lastName === '') {
         return '';
       }
-      return this.name + ' ' + 'Streeter';
+      return this.name + ' ' + this.lastName;
     }
   },
 
