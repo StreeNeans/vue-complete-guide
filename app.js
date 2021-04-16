42843840e8628b30e837e3d49fbfd2c0d2514efa
Nena Streeter <nenastreeter@gmail.com>
2021-04-16 14:01:43 -0500

    Box select toggles O: , classes, video 33

--- app.js
+++ app.js
@@ -1,73 +1,24 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
-      name: '',
-      lastName: '',
-      // fullName: '',
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
     };
   },
 
-  watch: {
-    counter(value) {
-      if (value > 50) {
-        const that = this;
-        setTimeout(function() {
-          that.counter = 0;
-        }, 2000);
-
-      }
-    },
-    // name(value) {
-    //   if(value === '') {
-    //     this.fullName = '';
-    //   } else {
-    //       this.fullName = value + ' ' + this.lastName;
-    //   }
-      
-    // },
-    // lastName(value) {
-    //   if(value === '') {
-    //     this.fullName = '';
-    //   } else {
-    //       this.fullName = this.name + ' ' + value;
-    //   }
-      
-    // },
-  },
-
-   computed: {
-    fullName() {
-      console.log ('Running again...');
-      if (this.name === '' || this.lastName === '') {
-        return '';
-      }
-      return this.name + ' ' + this.lastName;
-    },
-   },
-
   methods: {
-    outputFullName() {
-      console.log ('Running again...');
-      if (this.name === '') {
-        return '';
+    boxSelected(box) {
+      if (box === 'A') {
+        this.boxASelected = !this.boxASelected;
+      } else if (box === 'B') {
+        this.boxBSelected = !this.boxBSelected;
+      } else if (box === 'C') {
+        this.boxCSelected = !this.boxCSelected;
       }
-      return this.name + ' ' + 'Streeter';
-    },
-    setName(event, lastName) {
-      this.name = event.target.value;
-    },
-    add(num) {
-      this.counter = this.counter + num;
-    },
-    reduce(num) {
-      this.counter = this.counter - num;
-      // this.counter--;
-    },
-    resetInput() {
-      this.name = '';
-    },
+    }
   }
+
 });
 
-app.mount('#events');
+app.mount('#styling');
